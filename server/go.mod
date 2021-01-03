a56b35a65e9abc524c934ccffeab1c96dcc84bd9
satoya <mentol310@gmail.com>
2021-01-03 10:35:34 +0900

    add: server

--- server/go.mod
+++ server/go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.6.1
 	google.golang.org/grpc v1.34.0
+	google.golang.org/protobuf v1.25.0
 )
--- server/internal/di/di/adapter.go
+++ server/internal/di/di/adapter.go
@@ -2,16 +2,12 @@ package di
 
 import (
 	"github.com/somen440/flutter-go-todo/server/internal/adapter"
-	"github.com/somen440/flutter-go-todo/server/internal/adapter/helloworld"
-	"github.com/somen440/flutter-go-todo/server/internal/adapter/recorder"
 	"github.com/somen440/flutter-go-todo/server/internal/adapter/todo"
 )
 
 // NewAdapter return Adapter
 func NewAdapter() *adapter.Adapter {
 	return adapter.NewAdapter(
-		helloworld.NewHwAdapter(),
-		NewTodoAdapter(),
 		NewRecorderAdapter(),
 	)
 }
@@ -21,8 +17,4 @@ func NewTodoAdapter() *todo.TdAdapter {
 	return todo.NewTdAdapter(
 		NewTodoUsecase(),
 	)
-}
-
-func NewRecorderAdapter() *recorder.RecorderAdapter {
-	return recorder.NewRecorderAdapter()
 }
